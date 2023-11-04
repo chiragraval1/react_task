c48481a33b94676e7bcb041e11e4395b21ce4120
chiragraval1 <ravalchirag9599@gmail.com>
2023-11-04 14:03:39 +0530

    Nirav sir Task React

---
+++ task1open_model/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+          Onclick Open Image
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
