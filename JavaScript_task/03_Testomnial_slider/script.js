eb0f4da3576a729eb25b19456600524ad88f2277
chiragraval1 <ravalchirag9599@gmail.com>
2023-11-18 19:38:02 +0530

    Slider

---
+++ JavaScript_task/03_Testomnial_slider/script.js
@@ -0,0 +1,16 @@
+console.log("Hello dosto");
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
